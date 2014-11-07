4b1ff599ce157ce6d22f916c559c2a2023610da0
Tushar <Tushar@Tushars-MacBook-Pro.local>
2014-11-07 21:23:45 +0530

    So many changes

--- js/app.js
+++ js/app.js
@@ -197,6 +197,18 @@ angular.module('starter', ['ionic', 'ui.bootstrap', 'starter.controllers','ngCor
         }
     })
 
+    
+    .state('tab.disclaimer', {
+        url: '/setting/disclaimer',
+        views: {
+            'tab-setting': {
+                templateUrl: 'templates/disclaimer.html',
+                controller: 'HomeCtrl'
+            }
+        }
+    })
+
+    
     // if none of the above states are matched, use this as the fallback
     $urlRouterProvider.otherwise('/tab/home');
 
