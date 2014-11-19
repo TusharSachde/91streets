dfc1087d2f7ff237cc6dc863e44180fe80cccfbe
Tushar <Tushar@Tushars-MacBook-Pro.local>
2014-11-19 13:04:49 +0530

    trial

--- js/PushwooshAndroid.js
+++ js/PushwooshAndroid.js
@@ -21,6 +21,33 @@ function registerPushwooshAndroid() {
 
  	var pushNotification = window.plugins.pushNotification;
 
+    //set push notifications handler
+    document.addEventListener('push-notification', function(event) {
+        var title = event.notification.title;
+        var userData = event.notification.userdata;
+                                 
+        if(typeof(userData) != "undefined") {
+            console.warn('user data: ' + JSON.stringify(userData));
+        }
+                                     
+        alert(title);
+    });
+ 
+    //initialize Pushwoosh with projectid: "GOOGLE_PROJECT_ID", appid : "PUSHWOOSH_APP_ID". This will trigger all pending push notifications on start.
+    pushNotification.onDeviceReady({ projectid: "697329602980", appid : "F39B5-A73B5" });
+ 
+    //register for pushes
+    pushNotification.registerDevice(
+        function(status) {
+            var pushToken = status;
+            console.warn('push token: ' + pushToken);
+        },
+        function(status) {
+            console.warn(JSON.stringify(['failed to register ', status]));
+        }
+    );
+    
+    /*
 	//set push notifications handler
 	document.addEventListener('push-notification',
 		function(event)
@@ -60,7 +87,7 @@ function registerPushwooshAndroid() {
 			alert("Failed to register: " +  status);
 		    console.warn(JSON.stringify(['failed to register ', status]));
 		}
-	);
+	);*/
 }
 
 function onPushwooshAndroidInitialized(pushToken)
